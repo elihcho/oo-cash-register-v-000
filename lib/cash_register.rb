fea4b73f0e476189c80aff4ce9a673a74a1d9428
Eli Cho <eli.cho@gmail.com>
2018-08-08 22:10:09 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -13,7 +13,7 @@ class CashRegister
   
   def apply_discount
     @total *= 0.8
-    return "After the discount, the total comes to $#{@total.to_i}."
+    "After the discount, the total comes to $#{@total.to_i}."
   end
   
 end
